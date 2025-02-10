c5dc71670ef2e0e92217bdce38571ef639cc1ae0
BuildTools <unconfigured@null.spigotmc.org>
2025-02-10 23:11:22 +0200

    upload git hub

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,44 @@
+import 'package:electric_store_app/constants.dart';
+import 'package:electric_store_app/models/product.dart';
+import 'package:electric_store_app/widgets/details/details_body.dart';
+import 'package:flutter/material.dart';
+
+
+class DetailsScreen extends StatelessWidget {
+  final Product product;
+
+  const DetailsScreen({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      appBar: detailsAppBar(context),
+      body: DetailsBody(
+        product: product,
+      ),
+    );
+  }
+
+  AppBar detailsAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: kBackgroundColor,
+      elevation: 0,
+      leading: IconButton(
+        padding: EdgeInsets.only(right: kDefaultPadding),
+        icon: Icon(
+          Icons.arrow_back,
+          color: kPrimaryColor,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      centerTitle: false,
+      title: Text(
+        'رجوع',
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+    );
+  }
+}
